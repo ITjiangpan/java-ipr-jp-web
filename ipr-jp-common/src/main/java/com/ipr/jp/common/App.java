1374cd251c087d40dec74108f9984329e33a1b83
jiangpan <jiangpan@zbj.com>
2017-08-03 10:14:29 +0800

    初始化项目

---
+++ ipr-jp-common/src/main/java/com/ipr/jp/common/App.java
@@ -0,0 +1,13 @@
+package com.ipr.jp.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ipr-jp-dao/src/main/java/com/ipr/jp/dao/App.java
@@ -0,0 +1,13 @@
+package com.ipr.jp.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
